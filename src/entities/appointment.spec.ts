c6440898c4043ca43f12b534156729f744ee88be
MuhdHanish <muhammedhanish11@gmail.com>
2024-03-31 12:24:28 +0530

    added new test is appointment.spec.ts for condition throwing the error

--- src/entities/appointment.spec.ts
+++ src/entities/appointment.spec.ts
@@ -2,12 +2,32 @@ import { expect, test } from 'vitest'
 import { Appointment } from './appointment'
 
 test('create an appointment', () => {
+  const startsAt = new Date()
+  const endsAt = new Date()
+
+  endsAt.setDate(endsAt.getDate() + 1)
+
   const appointment = new Appointment({
     customer: 'Jhon Deo',
-    startsAt: new Date(),
-    endsAt: new Date()
+    startsAt,
+    endsAt
   })
 
   expect(appointment).toBeInstanceOf(Appointment)
   expect(appointment.customer).toEqual('Jhon Deo')
+})
+
+test('cannot create an appointment with end date before start date', () => {
+  const startsAt = new Date()
+  const endsAt = new Date()
+
+  endsAt.setDate(endsAt.getDate() - 1)
+
+  expect(() => {
+    return new Appointment({
+      customer: 'Jhon Doe',
+      startsAt,
+      endsAt
+    })
+  }).toThrow()
 })
