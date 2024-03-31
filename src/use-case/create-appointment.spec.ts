5f59d59c9e9725c8753b71c99e1ec765bb8770c2
MuhdHanish <muhammedhanish11@gmail.com>
2024-03-31 15:05:40 +0530

    added overlapping testing in create appointment test file

--- src/use-case/create-appointment.spec.ts
+++ src/use-case/create-appointment.spec.ts
@@ -1,19 +1,41 @@
 import { describe, expect, it } from 'vitest'
-import { CreateAppointment } from './create-appointment'
 import { Appointment } from '../entities/appointment'
+import { CreateAppointment } from './create-appointment'
 import { getFutureDate } from '../tests/utils/get-future-date'
+import { InMemoryAppointmentsRepository } from '../repositories/in-memory/in-memory-appointments-repository'
 
 describe('Create Appointment', () => {
   it('should be able to create an appointment', async () => {
-    const createAppointment = new CreateAppointment()
+    const appointmentsRepository = new InMemoryAppointmentsRepository()
+    const createAppointment = new CreateAppointment(appointmentsRepository)
 
-    const startsAt = getFutureDate('2024-08-10')
-    const endsAt = getFutureDate('2024-08-11')
+    const startsAt = getFutureDate('2024-10-01')
+    const endsAt = getFutureDate('2024-10-02')
 
     await expect(createAppointment.execute({
       customer: 'Jhon Deo',
       startsAt,
       endsAt
     })).resolves.toBeInstanceOf(Appointment)
+  })
+
+  it('should not be able to create an appointment with overlapping dates', async () => {
+    const appointmentsRepository = new InMemoryAppointmentsRepository()
+    const createAppointment = new CreateAppointment(appointmentsRepository)
+
+    const startsAt = getFutureDate('2024-10-01')
+    const endsAt = getFutureDate('2024-10-30')
+
+    await createAppointment.execute({
+      customer: 'Jhon Deo',
+      startsAt,
+      endsAt
+    })
+
+    await expect(createAppointment.execute({
+      customer: 'Jhon Deo',
+      startsAt: getFutureDate('2024-10-01'),
+      endsAt: getFutureDate('2024-10-30')
+    })).rejects.toBeInstanceOf(Error)
   })
 })
