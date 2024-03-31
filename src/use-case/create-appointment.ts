9110977baf88744b7d2f4ba85f24ce448d60bd1e
MuhdHanish <muhammedhanish11@gmail.com>
2024-03-31 15:05:10 +0530

    completed user repostiory implementation in use case

--- src/use-case/create-appointment.ts
+++ src/use-case/create-appointment.ts
@@ -1,4 +1,5 @@
 import { Appointment } from '../entities/appointment'
+import { type IAppointmentsRepository } from '../repositories/appointments-repository'
 
 interface ICreateAppointmentRequest {
   customer: string
@@ -9,8 +10,17 @@ interface ICreateAppointmentRequest {
 type TCreateAppointmentResponse = Appointment
 
 export class CreateAppointment {
+  constructor (
+    private readonly appointmentsRepository: IAppointmentsRepository
+  ) { }
+
   async execute (requestData: ICreateAppointmentRequest): Promise<TCreateAppointmentResponse> {
+    const overLappingAppointment = await this.appointmentsRepository.findOverLappingAppointment(requestData.startsAt, requestData.endsAt)
+    if (overLappingAppointment !== undefined) {
+      throw new Error('Another appointment overlaps this appointmet dates')
+    }
     const appointment = new Appointment(requestData)
+    await this.appointmentsRepository.create(appointment)
     return appointment
   }
 }
