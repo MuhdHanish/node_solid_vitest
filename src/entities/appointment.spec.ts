c754a152f4cccea4997c3ddea55249ef7f060255
MuhdHanish <muhammedhanish11@gmail.com>
2024-03-31 13:05:40 +0530

    created new test for new condition in appointment.spect.ts and changed every function with the new condition

--- src/entities/appointment.spec.ts
+++ src/entities/appointment.spec.ts
@@ -5,7 +5,8 @@ test('create an appointment', () => {
   const startsAt = new Date()
   const endsAt = new Date()
 
-  endsAt.setDate(endsAt.getDate() + 1)
+  startsAt.setDate(startsAt.getDate() + 1)
+  endsAt.setDate(endsAt.getDate() + 2)
 
   const appointment = new Appointment({
     customer: 'Jhon Deo',
@@ -21,7 +22,24 @@ test('cannot create an appointment with end date before start date', () => {
   const startsAt = new Date()
   const endsAt = new Date()
 
-  endsAt.setDate(endsAt.getDate() - 1)
+  startsAt.setDate(startsAt.getDate() + 2)
+  endsAt.setDate(endsAt.getDate() + 1)
+
+  expect(() => {
+    return new Appointment({
+      customer: 'Jhon Doe',
+      startsAt,
+      endsAt
+    })
+  }).toThrow()
+})
+
+test('cannot create an appointment with start date before now', () => {
+  const startsAt = new Date()
+  const endsAt = new Date()
+
+  startsAt.setDate(startsAt.getDate() - 1)
+  endsAt.setDate(endsAt.getDate() + 2)
 
   expect(() => {
     return new Appointment({
