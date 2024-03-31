c47e45980ecbaa5dca66ec940c0d2f5b3159bec2
MuhdHanish <muhammedhanish11@gmail.com>
2024-03-31 13:40:25 +0530

    used get future fuction in appointment test file

--- src/entities/appointment.spec.ts
+++ src/entities/appointment.spec.ts
@@ -1,12 +1,10 @@
 import { expect, test } from 'vitest'
 import { Appointment } from './appointment'
+import { getFutureDate } from '../tests/utils/get-future-date'
 
 test('create an appointment', () => {
-  const startsAt = new Date()
-  const endsAt = new Date()
-
-  startsAt.setDate(startsAt.getDate() + 1)
-  endsAt.setDate(endsAt.getDate() + 2)
+  const startsAt = getFutureDate('2024-08-10')
+  const endsAt = getFutureDate('2024-08-11')
 
   const appointment = new Appointment({
     customer: 'Jhon Deo',
@@ -19,11 +17,8 @@ test('create an appointment', () => {
 })
 
 test('cannot create an appointment with end date before start date', () => {
-  const startsAt = new Date()
-  const endsAt = new Date()
-
-  startsAt.setDate(startsAt.getDate() + 2)
-  endsAt.setDate(endsAt.getDate() + 1)
+  const startsAt = getFutureDate('2024-08-10')
+  const endsAt = getFutureDate('2024-08-09')
 
   expect(() => {
     return new Appointment({
@@ -39,7 +34,7 @@ test('cannot create an appointment with start date before now', () => {
   const endsAt = new Date()
 
   startsAt.setDate(startsAt.getDate() - 1)
-  endsAt.setDate(endsAt.getDate() + 2)
+  endsAt.setDate(endsAt.getDate() + 3)
 
   expect(() => {
     return new Appointment({
